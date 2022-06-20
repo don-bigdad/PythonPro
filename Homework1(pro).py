c28dda10b8565b4537b99c1443cfa54b6cb3223e
Bogdan <97698639+don-bigdad@users.noreply.github.com>
2022-06-20 15:17:04 +0300

    Update Homework1(pro).py

--- Homework1(pro).py
+++ Homework1(pro).py
@@ -18,24 +18,27 @@ product1=Product(20,"Apple")
 product2=Product(30,"banana")
 product3=Product(35,"cherry")
 product_list=[(product1.name,product1.price),(product2.name,product2.price),(product3.name,product3.price)]
-quantyties={}
+
 class Order:
-    def __init__(self,person:Person,product:Product):
+    
+    def __init__(self,person:Person):
         self.person=person
-        self.product=product
+        self.product=[]
+        self.quantyties={}
     def append(self,product_name,count):
-        quantyties[product_name]=count
-        return quantyties
+        self.quantyties[product_name]=count
+        return self.quantyties
     def to_pay(self):
         sum=0
-        for element in quantyties:
+        for element in self.quantyties:
             for item in product_list:
                 if item[0]==element:
-                    sum+=quantyties[element]*item[1]
+                    sum+=self.quantyties[element]*item[1]
         return sum
     def __str__(self):
-        return f'Person {self.person} has order {" and ".join([elem for elem in quantyties])}.Total price:{self.to_pay()}'
-order1=Order(person1,product1)
+        return f'Person {self.person} ordered {" and ".join([elem for elem in self.quantyties])}.Total price:{self.to_pay()}'
+order1=Order(person1)
 order1.append("cherry",3)
 order1.append("Apple",5)
+order1.append("banana",2)
 print(order1)
