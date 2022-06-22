014f88e66e7ebc9180cc65e19cd16014ae0938e8
don-bigdad <bogdan.maksimenko.2002@gmail.com>
2022-06-22 14:11:02 +0300

    Доработать методы, чтобы все работало через return

--- Homework2.py
+++ Homework2.py
@@ -1,65 +1,62 @@
 
-class Human:
-    def __init__(self, name, surname, patronymic, age):
-        self.name = name
-        self.surname = surname
-        self.patronymic = patronymic
-        self.age = age
-
+class Person:
+    def __init__(self,surname:str,name:str):
+        self.surname=surname
+        self.name=name
     def __str__(self):
-        return f'{self.name} {self.surname[0]}.{self.patronymic[0]}. {self.age}'
-
-
-student_1 = Human("Ivanov", "Ivan", "Ivanich", 18)
-print(student_1)
-
-
-class Student(Human):
-    def __init__(self, surname, name, patronomyc, mail,age):
-        super().__init__(surname, name, patronomyc,age)
-        self.mail = mail
-
+        return f'{self.surname} {self.name}'
+class Student(Person):
+    def __init__(self,surname:str,name:str,age:int,number:str):
+        super().__init__(surname,name)
+        self.age=age
+        self.number=number
     def __str__(self):
-        return f'ФИО :{self.name} {self.surname} {self.patronymic[0]}. Mail: ' \
-               f'{self.mail},age {self.age}'
+        return f'{super().__str__()} {self.age} {self.number}'
+person1=Person("Ivanov1","Ivan")
 
+student1=Student("Ivanov1","Ivan",18,"123456789")
+student2=Student("Ivanov2","Ivan",18,"123456789")
+student3=Student("Ivanov3","Ivan",18,"123456789")
+student4=Student("Ivanov4","Ivan",18,"123456789")
+student5=Student("Ivanov5","Ivan",18,"123456789")
+student6=Student("Ivanov6","Ivan",18,"123456789")
+student7=Student("Ivanov7","Ivan",18,"123456789")
+student8=Student("Ivanov8","Ivan",18,"123456789")
+student9=Student("Ivanov9","Ivan",18,"123456789")
+student10=Student("Ivanov10","Ivan",18,"123456789")
+student11=Student("Ivanov11","Ivan",18,"123456789")
 
-student1 = Student("Ivanov", "Ivan", "Ivanich", "somemail1@gmail.com",18)
-student2 = Student("Petrov", "Ivan", "Ivanich", "somemail1@gmail.com",18)
-student3 = Student("Sergeyev", "Ivan", "Ivanich", "somemail1@gmail.com",18)
-student4 = Student("Bogdanov", "Ivan", "Ivanich", "somemail1@gmail.com",18)
-student5 = Student("Danilov", "Ivan", "Ivanich", "somemail1@gmail.com",18)
-student6 = Student("Rodionov", "Ivan", "Ivanich", "somemail1@gmail.com",18)
-print(student1)
-class Group():
-    student_list = []
-
-    def add_student(self, student):
-        if len(self.student_list)<5:
-            self.student_list.append(student.name)
+class Group:
+    group=[]
+    def add_student(self,student:Student):
+        if len(self.group)<10:
+            self.group.append(student.surname+" "+student.name)
         else:
-            print( 'the group is full')
-    def del_student(self, student):
-        if len(self.student_list)>2:
-            self.student_list.remove(student)
-        else:
-            print("Group must have at least one student")
-    def search_student(self, surname):
-        for element in self.student_list:
-            if element in self.student_list:
-                print("Student in list")
-                break
-            else:
-                print("Not found")
-                break
+            return "Group is full"
+    def remove_student(self,student:Student):
+        for element in self.group:
+            if element.split()[0]==student.surname:
+                self.group.remove(element)
+    def search_by_surname(self,student:Student):
+        for elem in self.group:
+            if elem.split()[0]==student.surname:
+                print(elem + " in group")
+
     def __str__(self):
-       return f'{self.student_list} '
-group1=Group()
-group1.add_student(student3)
-group1.add_student(student2)
-group1.add_student(student5)
-group1.add_student(student4)
-group1.add_student(student2)
-group1.add_student(student1)
-group1.search_student(student2.surname)
-print(group1)
+        return f'{self.group}' # Чтобы без принта работало,но не знаю какой аргумент передавать в скобки {self.search_by_surname(????)}
+gr_1=Group()
+gr_1.add_student(student1)
+gr_1.add_student(student2)
+gr_1.add_student(student3)
+gr_1.add_student(student4)
+gr_1.add_student(student5)
+gr_1.add_student(student6)
+gr_1.add_student(student7)
+gr_1.add_student(student8)
+gr_1.add_student(student9)
+gr_1.add_student(student10)
+gr_1.add_student(student11)
+gr_1.remove_student(student1)
+gr_1.remove_student(student2)
+gr_1.search_by_surname(student4)
+print(gr_1)
