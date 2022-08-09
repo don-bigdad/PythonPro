25633d8c3a6f45b60e9c5132691a1d14fd01e286
don-bigdad <bogdan.maksimenko.2002@gmail.com>
2022-08-10 01:40:39 +0300

    completed the mail validated

--- regular exp.py
+++ regular exp.py
@@ -2,16 +2,14 @@ import re
 
 # valid card number function
 def valid_card(card_number):
-    pattern = r'^\d{4}-\d{4}-\d{4}-\d{4}$'
-    check1 = re.search(pattern, my_card)
-    return card_number if check1 else 'Invalid card number'
+    return card_number if re.search(r'^\d{4}-\d{4}-\d{4}-\d{4}$', my_card) else 'Invalid card number'
 my_card = "1233-5213-9990-7622"
 print(valid_card(my_card))
 # Напишите регулярное выражение, которое будет находить в тексте фрагменты,
 # состоящие из одной буквы R, за которой следует одна или более букв b,
 # за которой одна r. Учитывать верхний и нижний регистр.
-in_string = r'\w[Rb+r]'
-my_string = "asdRbbbbrlsk"
+in_string = r'\w[Rb+r]$'
+my_string = "asRbbbr"
 match = re.search(in_string, my_string)
 print(match.string) if match else print("Something wrong")
 # Напишите функцию, принимающую строковые данные и выполняющую проверку на их соответствие мейлу.
@@ -21,14 +19,10 @@ print(match.string) if match else print("Something wrong")
 # -в теле мейла допустимы только символы “_” и “-”. Но они не могут быть первым символом мейла.
 # -символ “-” не может повторяться.
 def valid_mail(mail):
-    valid=r''
-    compare=re.search(valid,mail)
-    return mail if compare else 'Invalid mail'
+    return mail if re.search(r'^[^-|_]\w*[-{0,1}]?\w*?[@]?\w*(.com)?$',mail) else 'Invalid mail'
+print(valid_mail("mymail31-2@gmail.com"))
 # Напишите функцию, проверяющую правильность логина.
 # Правильный логин – строка от 2 до 10 символов, содержащая только буквы и цифры.
 def valid_log(login):
-    valid_login = r'^(\w|\d){2,10}$'
-    compare = re.search(valid_login, login)
-    return login if compare else 'Invalid login'
-
-print(valid_log("adD32xFx"))
+    return login if re.search(r'^(\w|\d){2,10}$', login) else 'Invalid login'
+print(valid_log("adDa2xFx"))
